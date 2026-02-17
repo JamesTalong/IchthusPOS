fb5bbcdd78afabe30fda2cb6b630cdb809a06881
JamesTalong <jamesdelasalas21@gmail.com>
2026-02-17 13:53:25 +0800

    complete file

---
+++ ichthus/src/security.jsx
@@ -0,0 +1,7 @@
+//export const domain = `http://192.168.45.105:8021`; // server Malabon
+//export const domain = `http://114.29.239.77:8002`; // local
+//export const domain = `http://192.168.10.222:8021`; // Batangas
+//export const domain = `http://localhost:8002`; // Temporary local
+export const domain = `http://114.29.239.77:8002`;
+
+export const domainMark = `http://192.168.150.40:7251`;
